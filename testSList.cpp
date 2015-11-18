611af475b2c9d0a853fff46669b22dfebdcec3ba
stefobark <steven.j.barker.jr@gmail.com>
2015-11-18 08:06:34 -0800

    fixed testSList

--- testSList.cpp
+++ testSList.cpp
@@ -13,9 +13,7 @@ int main(){
 	cout << "\ndemonstrating the size method: " << list.size() << "\n\n";
 	cout << "we also used a method called 'addValueIfNotPresent' to try \nto add 3 again, and we also added a 4 \nso, there should only be one 3 and a 4..." << "\n\n";
 	cout << "looking for value 4. \nShould return 0 if not present, 1 if it is present. \nLet's see: " << list.checkVal(4) << "\n\n";
-	cout << "now we're checking if a value is there and removing it\n\n";
+	cout << "now we're checking if a value (4) is there and removing it\n\n";
 	list.removeValueIfPresent(4);
-	list.printList();
-	list.sort();
 	list.printList();
 }
