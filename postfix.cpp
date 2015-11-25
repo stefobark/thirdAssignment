38b3bd7b48d9c0be5734aac0a0610a85ef96bf79
stefobark <steven.j.barker.jr@gmail.com>
2015-11-25 05:57:56 -0800

    better explanation of postfix eval

--- postfix.cpp
+++ postfix.cpp
@@ -13,34 +13,35 @@ int main(){
 		if(isdigit(itr)){
 			int pushDigit = itr - '0';
 			pFStack.push(pushDigit);
-			cout << "top = " << pFStack.top() << "\n";
+			cout << "found operand, pushing to top = " << pFStack.top() << "\n";
 		}
 		
 		if(itr == '*'){
 			
 			int first = pFStack.top();
-			cout << "multiplying " << first << " ";
+			cout << "found operator, multiplying " << first << " ";
 			pFStack.pop();
 			int second = pFStack.top();
 			cout << "by " << second << " = ";
 			pFStack.pop();
 			int mPushThis = first * second;
-			cout << "i get " << mPushThis << endl;
+			cout << "i get " << mPushThis << " and push it to the stack" << endl;
 			pFStack.push(mPushThis);
 		}
 		
 		if(itr == '+'){
 			int addFirst = pFStack.top();
-			cout << "adding " << addFirst << " ";
+			cout << "found operator, adding " << addFirst << " ";
 			pFStack.pop();
 			int addSecond = pFStack.top();
 			cout << "and " << addSecond << " = ";
 			pFStack.pop();
 			int aPushThis = addFirst + addSecond;
-			cout << aPushThis << endl;
+			cout << aPushThis << " and push it to the stack" << endl;
 			pFStack.push(aPushThis);
 		}
 			//cout << itr << endl;
 	}
 	cout << "should be 288 (followed explanation of algorithm from book): "<< pFStack.top() << endl; 
 }
+				
